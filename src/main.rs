d3ca2485749ba8daad45d1592a3beada132f0a46
mol power <60449021+zxrs@users.noreply.github.com>
2026-06-08 08:53:30 +0900

    impl choose language (#1)

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,21 @@ use std::{env, fs, io::Read, path::PathBuf, ptr, slice};
 use anyhow::{Context, Result};
 use pdfium_render::prelude::*;
 use windows::{
+    Globalization::Language,
     Graphics::Imaging::{BitmapBufferAccessMode, BitmapPixelFormat, SoftwareBitmap},
     Media::Ocr::OcrEngine,
     Win32::System::WinRT::IMemoryBufferByteAccess,
-    core::Interface,
+    core::{HSTRING, Interface},
 };
 
 /// 対象の PDF に頻出する文字列の辞書（調整要）
 const DICTIONARY: &[&str] = &[
     "検査", "成績", "寸法", "製造", "合格", "工場", "図面", "番号", "日付", "規格", "証明", "会社",
-    "引張", "試験", "位置", "材質", "公差", "外観", "INSPE", "Inspe", "RESUL", "Resul", "DIMEN",
-    "Dimen", "JOB", "Job", "ACCEP", "Accep", "WORKS", "Works", "DATE", "Date", "SPECI", "Speci",
-    "CERTI", "Certi", "COMPA", "Compa", "TENSI", "Tensi", "TEST", "Test", "POSIT", "Posit",
-    "MATER", "Mater", "VISUA", "Visua",
+    "引張", "試験", "位置", "材質", "公差", "外観", "inspec", "resul", "dimens", "job", "accep",
+    "works", "date", "speci", "certi", "compa", "tensil", "test", "posit", "mater", "visua",
 ];
-/// 辞書に登録されている文字列が見つかったときに使用する係数（調整要）
-const FACTOR: usize = 8;
+/// 辞書に登録されている文字列が見つかったときに使用するしきい値（調整要）
+const THRESHOLD: usize = 8;
 
 /// 指定された PdfPageRenderRotation で PdfPage をレンダリングし、画像の幅、高さ、ビットマップバッファを返す
 fn render(page: &PdfPage, rotation: PdfPageRenderRotation) -> Result<(u32, u32, Vec<u8>)> {
@@ -36,7 +35,7 @@ fn render(page: &PdfPage, rotation: PdfPageRenderRotation) -> Result<(u32, u32, 
 }
 
 /// レンダリングされた画像の幅、高さ、バッファを受け取って、OCR した結果の文字列を返す
-fn ocr(width: u32, height: u32, buf: Vec<u8>) -> Result<String> {
+fn ocr(width: u32, height: u32, buf: Vec<u8>, tag: &HSTRING) -> Result<String> {
     let bmp = SoftwareBitmap::Create(BitmapPixelFormat::Bgra8, width as i32, height as i32)?;
     {
         let bmp_buf = bmp.LockBuffer(BitmapBufferAccessMode::Write)?;
@@ -51,7 +50,8 @@ fn ocr(width: u32, height: u32, buf: Vec<u8>) -> Result<String> {
         let slice = unsafe { slice::from_raw_parts_mut(data, capacity as usize) };
         slice.clone_from_slice(&buf);
     }
-    let engine = OcrEngine::TryCreateFromUserProfileLanguages()?;
+    let lang = Language::CreateLanguage(tag)?;
+    let engine = OcrEngine::TryCreateFromLanguage(&lang)?;
     let result = engine
         .RecognizeAsync(&bmp)?
         .join()?
@@ -59,13 +59,14 @@ fn ocr(width: u32, height: u32, buf: Vec<u8>) -> Result<String> {
         .to_string_lossy()
         .chars()
         .filter(|s| s != &' ')
-        .collect();
+        .collect::<String>()
+        .to_lowercase();
 
     Ok(result)
 }
 
 /// PdfPage を 90 degrees ずつ回転させて、認識された文字数が最も多かった PdfPageRenderRotation を返す
-fn detect(page: &PdfPage) -> Result<PdfPageRenderRotation> {
+fn detect(page: &PdfPage, tag: &HSTRING) -> Result<PdfPageRenderRotation> {
     let rotation = [
         PdfPageRenderRotation::None,
         PdfPageRenderRotation::Degrees90,
@@ -75,11 +76,12 @@ fn detect(page: &PdfPage) -> Result<PdfPageRenderRotation> {
     .into_iter()
     .filter_map(|r| {
         let (width, height, buf) = render(page, r).ok()?;
-        let ocr_str = ocr(width, height, buf).ok()?;
+        let ocr_str = ocr(width, height, buf, tag).ok()?;
+        // dbg!(&ocr_str);
         let hit = DICTIONARY.iter().filter(|s| ocr_str.contains(*s)).count();
         // 辞書に登録されている文字が見つかった場合は結果に係数を掛けて優先度を上げる
         let result = if hit > 0 {
-            ocr_str.len() * (1 + 1 / FACTOR * hit)
+            ocr_str.len() * (1 + 1 / THRESHOLD * hit)
         } else {
             ocr_str.len()
         };
@@ -96,13 +98,33 @@ fn start() -> Result<()> {
         .nth(1)
         .map(PathBuf::from)
         .context("PDF ファイルが見つかりません。")?;
+
+    let lang_tags = OcrEngine::AvailableRecognizerLanguages()?
+        .First()?
+        .filter_map(|l| Some((l.DisplayName().ok()?, l.LanguageTag().ok()?)))
+        .collect::<Vec<_>>();
+
+    let tag = if lang_tags.len() == 1 {
+        &lang_tags[0].1
+    } else if lang_tags.len() > 1 {
+        println!("対象の言語を選択してください。");
+        for (i, (name, _)) in lang_tags.iter().enumerate() {
+            println!("{}: {}", i + 1, name);
+        }
+        let mut buf = String::new();
+        std::io::stdin().read_line(&mut buf)?;
+        &lang_tags[buf.trim().parse::<usize>()? - 1].1
+    } else {
+        panic!("No availavle recognizer language");
+    };
+
     let pdf = fs::read(&arg)?;
     let pdfium = Pdfium::default();
     let src_doc = pdfium.load_pdf_from_byte_vec(pdf, None)?;
     let mut dst_doc = pdfium.create_new_pdf()?;
     for (i, page) in src_doc.pages().iter().enumerate() {
         println!("[{:>3}/{:>3}] ページを処理中", i + 1, src_doc.pages().len());
-        let rotation = detect(&page)?;
+        let rotation = detect(&page, tag)?;
         // dbg!(page.rotation()?);
         // dbg!(rotation);
         let len = dst_doc.pages().len();
